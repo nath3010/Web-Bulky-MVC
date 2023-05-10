8d973b1dcde99d6e8fe278b56a9b378c02082ff3
nathalia <nathalia.dias3010@gmail.com>
2023-05-10 20:59:56 -0300

    Adj Ntier arch

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAcess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+            
+        }
+
+        //create table
+        public DbSet<Category> Categories { get; set; }
+
+        //add data to the table Category
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //add data to the table Category
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
