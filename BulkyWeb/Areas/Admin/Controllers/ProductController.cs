b8e12015877409abede5fc638fe76609f848cdac
nathalia <nathalia.dias3010@gmail.com>
2023-05-19 22:53:57 -0300

    add upsert and add img file save

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -11,10 +11,13 @@ namespace BulkyWeb.Areas.Admin.Controllers
 	public class ProductController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
+		private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+			_webHostEnvironment = webHostEnvironment;
+			
         }
         public IActionResult Index()
 		{
@@ -23,7 +26,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
 			return View(objproductsList);
 		}
 
-		public IActionResult Create() 
+		public IActionResult Upsert(int? id) //Update and Insert 
 		{
 			//Projections in EF Core
 			IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
@@ -44,17 +47,40 @@ namespace BulkyWeb.Areas.Admin.Controllers
 				CategoryList = CategoryList,
 				Product = new Product()
 			};
+			if(id ==  null ||  id == 0)
+			{
+				//create
+				return View(productVM);
+			}
+			else
+			{
+				//update
+				productVM.Product = _unitOfWork.Product.Get(u=>u.Id == id);
+				return View(productVM);
+			}
 
-
-			return View(productVM);
+			
 		}
 
 		[HttpPost]
-		public IActionResult Create(ProductVM obj) 
+		public IActionResult Upsert(ProductVM productVM, IFormFile? file) 
 		{
 			if (ModelState.IsValid)
 			{
-				_unitOfWork.Product.Add(obj.Product);
+				string wwwRootPath = _webHostEnvironment.WebRootPath;
+				if (file != null)
+				{
+					string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+					string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+					using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+					{
+						file.CopyTo(fileStream);
+					}
+
+					productVM.Product.ImgUrl = @"\images\product\" + fileName;
+				}
+				_unitOfWork.Product.Add(productVM.Product);
 				_unitOfWork.Save();
 				TempData["success"] = "Product created successfully";
 
@@ -63,45 +89,17 @@ namespace BulkyWeb.Areas.Admin.Controllers
 			else
 			{
 				//Projections in EF Core
-				obj.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+				productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
 				{
 					Text = u.Name,
 					Value = u.Id.ToString()
 				});
-				return View(obj);
+				return View(productVM);
 			}
 			
 		}
-		public IActionResult Edit(int? id)
-		{
-			if(id == null || id == 0) 
-			{
-				return NotFound();
-			}
+	
 
-			Product? productDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-			if (productDb == null)
-			{
-				return NotFound();
-			}
-			return View(productDb);
-		}
-
-		[HttpPost]
-		public IActionResult Edit(Product obj) 
-		{
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Product.Update(obj);
-				_unitOfWork.Save();
-
-				TempData["success"] = "Product edit successfully";
-
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
 
 		public IActionResult Delete(int id) 
 		{
